a0c357e1f23e8254570bda1d42cf544ecb83ac4f
Gooseek <pyfocok@gmail.com>
2020-05-11 11:23:57 +0000

    added html and js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
-const link = document.querySelector('a');
+const link = document.querySelector("a");
 const display = document.querySelector(".display");
 
-document.querySelectorAll(".digits button")
-    .forEach(button => button.addEventListener("click", digitPressed));
+document
+    .querySelectorAll(".digits button")
+    .forEach((button) => button.addEventListener("click", digitPressed));
 
 function digitPressed(ev) {
     display.value += ev.target.innerText;
 }
 
-document.querySelectorAll(".opers button")
-    .forEach(button => button.addEventListener("click", opersPressed));
+document
+    .querySelectorAll(".opers button")
+    .forEach((button) => button.addEventListener("click", opersPressed));
 
 function opersPressed(ev) {
     display.value += ev.target.innerText;
 }
 
-document.querySelector(".eq").addEventListener("click", culculated);
+document
+    .querySelector(".eq").addEventListener("click", culculated);
 
 function culculated() {
     display.value = eval(display.value);
 }
 
-document.querySelector(".delete").addEventListener("click", deleted);
-
-function cdeleted() {
-    display.value = (display.value);
-}
+const digits = document.getElementsByClassName("digits");
